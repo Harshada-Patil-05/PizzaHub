6c5b6f1b4144bda7a1c5bdcddad7bfca91d8d947
Harshada Patil <harshupatil0405@gmail.com>
2026-02-13 00:48:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Layout from "./components/layout/Layout";
-import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <Toaster
-          position="top-center"
-          toastOptions={{
-            style: {
-              marginTop: "90px",
-            },
-          }}
+        position="top-center"
+        toastOptions={{
+          style: {
+            marginTop: "90px",
+          },
+        }}
       />
 
       <Layout>
